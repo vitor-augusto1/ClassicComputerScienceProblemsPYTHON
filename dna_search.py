e85f1b34c6c64c1fd829d44a2b6ebf568a1f1cae
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-06 15:01:19 -0300

    feat: Implement binary search

--- dna_search.py
+++ dna_search.py
@@ -1,3 +1,4 @@
+import keyword
 from enum import IntEnum
 from typing import Tuple, List
 
@@ -26,10 +27,25 @@ def linear_contains(gene: Gene, key_codon: Codon) -> bool:
     return False
 
 
+def binary_contains(gene: Gene, key_codon: Codon) -> bool:
+    low: int = 0
+    high: int = len(gene) - 1
+    while low <= high: # While there is a space to search
+        mid: int = (low + high) // 2
+        if gene[mid] < key_codon:
+            low = mid - 1
+        elif gene[mid] > key_codon:
+            high = mid - 1
+        else:
+            return True
+    return False
+
+
 my_gene: Gene = string_to_gene(gene_str)
 
 acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
 gat: Codon = (Nucleotide.G, Nucleotide.A, Nucleotide.T)
 
-print(linear_contains(my_gene, acg))
-print(linear_contains(my_gene, gat))
+my_sorted_gene: Gene = sorted(my_gene)
+print(binary_contains(my_sorted_gene, acg))
+print(binary_contains(my_sorted_gene, gat))
