ea28417e3de5f71965e1a5c234d67516137bc28b
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-28 11:10:27 -0300

    feat: Implement priority queue

--- generic_search.py
+++ generic_search.py
@@ -143,6 +143,24 @@ def bfs(initial: T, goal_test: Callable[[T], bool],
     return None
 
 
+class PriorityQueue(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+
+    @property
+    def empty(self) -> bool:
+        return not self._container
+
+    def push(self, item: T) -> None:
+        heappush(self._container, item)  # insert according to priority
+
+    def pop(self) -> T:
+        return heappop(self._container)  # remove according to priority
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+
 if __name__ == "__main__":
     print(linear_contains([1, 5, 15, 15, 15, 15, 20], 5))
     print(binary_contains(['a', 'd', 'e', 'f', 'z'], 'f'))
