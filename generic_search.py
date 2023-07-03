45c01af1c771a2b2ccfda342d54ee0037ebbb05f
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-07-03 14:45:56 -0300

    feat: Implement `A*` algorithm

--- generic_search.py
+++ generic_search.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from typing import (Deque, Optional, TypeVar, Iterable, Sequence, Generic, List,
+import concurrent
+from typing import (Deque, Dict, Optional, TypeVar, Iterable, Sequence, Generic, List,
                     Callable, Set, Deque, Any, Optional)
 from typing_extensions import Protocol
 from heapq import heappush, heappop
@@ -159,6 +160,24 @@ class PriorityQueue(Generic[T]):
 
     def __repr__(self) -> str:
         return repr(self._container)
+
+
+def astar(initial: T, goal_test: Callable[[T], bool], sucessors: Callable[[T],
+          List[T]], heuristic: Callable[[T], float]) -> Optional[Node[T]]:
+    frontier: PriorityQueue[Node[T]] = PriorityQueue()
+    frontier.push(Node(initial, None, 0.0, heuristic(initial)))
+    explored: Dict[T, float] = {initial: 0.0}
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        if goal_test(current_state):
+            return current_node
+        for child in sucessors(current_state):
+            new_cost: float = current_node.cost + 1
+            if child not in explored or explored[child] > new_cost:
+                explored[child] = new_cost
+                frontier.push(Node(child, current_node, new_cost, heuristic))
+    return None
 
 
 if __name__ == "__main__":
